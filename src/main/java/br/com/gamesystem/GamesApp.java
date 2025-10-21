7a2d27078852b603d037b9bd16cb1fc5e9936494
Guilherme Nunes <guilherme.augd@gmail.com>
2025-10-21 14:30:47 -0300

    create connection factory

--- src/main/java/br/com/gamesystem/GamesApp.java
+++ src/main/java/br/com/gamesystem/GamesApp.java
@@ -1,11 +1,20 @@
 package br.com.gamesystem;
 
+import br.com.gamesystem.factory.ConnectionFactory;
 import br.com.gamesystem.model.Game;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class GamesApp {
     public static void main(String[] args) {
 
-        Game tlou =  new Game("The Last Of Us", false, 299);
-
+        System.out.println("Testing connection...");
+        try {
+            Connection connection = ConnectionFactory.getConnection();
+            System.out.println("Success!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/br/com/gamesystem/factory/ConnectionFactory.java
@@ -0,0 +1,15 @@
+package br.com.gamesystem.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
+    private static final String USER = "RM562938";
+    private static final String PASSWORD = "250191";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
