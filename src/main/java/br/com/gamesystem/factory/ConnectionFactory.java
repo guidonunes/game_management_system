e70fdb5ef5ebc1e15cd0b982847077a2c07c8a28
Guilherme Nunes <guilherme.augd@gmail.com>
2025-10-21 16:25:03 -0300

    reset password

--- src/main/java/br/com/gamesystem/factory/ConnectionFactory.java
+++ src/main/java/br/com/gamesystem/factory/ConnectionFactory.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-    private static final String USER = "RM562938";
-    private static final String PASSWORD = "250191";
+    private static final String USER = "RM00000";
+    private static final String PASSWORD = "00000";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
